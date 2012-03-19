d261c8654b947b333b5b4c40222c116be339bfeb
Alexis Gallagher <alexis@alexisgallagher.com>
2012-03-19 14:50:03 +0000

    Initial Commit

---
+++ ZoomCrossDissolveTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZoomCrossDissolveTest
+//
+//  Created by Alexis Gallagher on 2012-03-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZoomCrossDissolveTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZoomCrossDissolveTest
+//
+//  Created by Alexis Gallagher on 2012-03-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
